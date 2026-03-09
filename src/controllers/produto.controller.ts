cebd310ed8a8e563d01347ed0edc9562010d8bd7
Júlia de Lima <maria.julial2514@gmail.com>
2026-03-09 20:33:19 -0300

    🐛 FIX: Correção tabela.

---
+++ src/controllers/produto.controller.ts
@@ -0,0 +1,62 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { ProdutoService } from "../services/produto.service";
+import { Produtos } from "../entities/produto.entity";
+
+
+
+
+@Controller("/produtos")
+export class ProdutoController{
+
+    constructor(
+        private readonly produtoService: ProdutoService
+    ){};
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Produtos[]> {
+    return this.produtoService.findAll();
+
+    }
+
+     @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Produtos> {
+    return this.produtoService.findById(id);
+    
+    }
+
+     @Get('/titulo/:titulo')
+    @HttpCode(HttpStatus.OK)
+    findAllByTitulo(@Param('titulo') titulo: string): Promise<Produtos[]> { // UTILIZANDO COLCHETES [] (ARRAY), POIS PODE SER QUE TENHAM MAIS TITULOS PARECIDOS
+    return this.produtoService.findAllByTitulo(titulo);
+    
+    }
+     @Get('/categoria')
+    @HttpCode(HttpStatus.OK)
+    findAllByCategoria(@Param('titulo') categoria: string): Promise<Produtos[]> { // UTILIZANDO COLCHETES [] (ARRAY), POIS PODE SER QUE TENHAM MAIS TITULOS PARECIDOS
+    return this.produtoService.findAllByCategoria(categoria);
+    
+    }
+     
+       @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() produto: Produtos): Promise<Produtos>{
+        return this.produtoService.create(produto);
+    }
+
+       @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() postagem: Produtos): Promise<Produtos>{
+        return this.produtoService.update(postagem);
+    }
+
+        @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number) {
+    return this.produtoService.delete(id);
+    
+    }
+
+
+}
--- src/services/produto.service.ts
+++ src/services/produto.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import { Produtos } from "../entities/produto.entity";
 
  
@@ -9,7 +9,10 @@ import { Produtos } from "../entities/produto.entity";
 
  @Injectable()
  export class ProdutoService{
-
+     findAllByCategoria(categoria: string): Promise<Produtos[]> {
+         throw new Error("Method not implemented.");
+     }
+   
     constructor (
         @InjectRepository(Produtos)
         private produtoRepository: Repository<Produtos>,
@@ -22,4 +25,55 @@ import { Produtos } from "../entities/produto.entity";
 
     }
 
+     async findById(id: number): Promise<Produtos> {
+
+         const produto = await this.produtoRepository.findOne({
+            where:{
+                id
+            }
+         })
+
+         
+         if (!produto)
+      throw new HttpException('Filme não encontrado!', HttpStatus.NOT_FOUND);
+
+         return produto;
+
  }
+
+    async findAllByTitulo(titulo: string): Promise<Produtos[]>{
+
+            // SELECT * FROM tb_postagens   WHERE titulo LIKE '%?%';
+            return this.produtoRepository.find({ // Usando ILike para ignorar letras maiúsculas e minúsculas
+                where:{
+                    titulo: ILike(`%${titulo}%`)
+                }
+            })
+         }
+
+
+           async create(produto: Produtos): Promise<Produtos>{
+            // INSERT INTO tb_postagens (titulo, texto) VALUES (valores digitados pelo usuario);
+            return await this.produtoRepository.save(produto);
+} 
+
+      async update(produto: Produtos): Promise<Produtos>{
+
+            if (!produto.id || produto.id <= 0)
+                throw new HttpException("Id inválido!", HttpStatus.BAD_REQUEST);
+            await this.findById(produto.id);
+            // UPDATE tb_postagens SET titulo = o que digitar, texto = ?, data = CURRENT_TIMESTAMP() WHERE id = que colocar;
+            return this.produtoRepository.save(produto);
+}  
+
+
+    async delete(id: number): Promise<DeleteResult>{
+        await this.findById(id);
+        // DELETE tb_postagens FROM id = ?;
+        return this.produtoRepository.delete(id);
+    }
+
+
+
+
+}
